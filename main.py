6b607dfee7927786fdd8b68cf469bec2b2d2dbba
aPrompter <istudyai@163.com>
2023-05-04 20:11:07 +0800

    feat: 添加文章通过GPT的API修改的能力

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import tkinter as tk
+from tkinter.simpledialog import askstring
+import tkinter.messagebox
 import openai
 
-# 配置OpenAI API密钥
-openai.api_key = "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"
+openai.api_key = "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"
 
 
-# 定义一个函数，当点击按钮时调用OpenAI API
 def generate_text():
     prompt = input_box.get()
     if prompt.strip() != "":
@@ -22,22 +22,41 @@ def generate_text():
         output_box.insert(tk.END, output_text)
 
 
-# 创建一个Tkinter窗口
+def edit_text(event):
+    ctrl_pressed = event.state & 0x4  # 检查Ctrl键是否按下
+    if ctrl_pressed and event.keysym == 'q':  # 检查是否按下了Ctrl + Q
+        selected_text = output_box.get(tk.SEL_FIRST, tk.SEL_LAST)
+        if selected_text:
+            suggestion = askstring("修改建议", "请输入修改意见：")
+            if suggestion:
+                prompt = f"{selected_text} -> {suggestion}"
+                print(f"{selected_text} -> {suggestion}")
+                response = openai.Completion.create(
+                    engine="text-davinci-003",
+                    prompt=prompt,
+                    max_tokens=4000 - len(prompt),
+                    n=1,
+                    stop=None,
+                    temperature=0.7,
+                )
+                new_text = response.choices[0].text
+                if tk.messagebox.askyesno("替换确认", f"原文本：{selected_text}\n新文本：{new_text}\n是否替换？"):
+                    output_box.delete(tk.SEL_FIRST, tk.SEL_LAST)
+                    output_box.insert(tk.INSERT, new_text)
+
+
 root = tk.Tk()
 root.title("AI写作软件")
 root.geometry("800x600")
 
-# 创建输入框
 input_box = tk.Entry(root, width=80)
 input_box.pack(pady=10)
 
-# 创建确认按钮
 confirm_button = tk.Button(root, text="生成文本", command=generate_text)
 confirm_button.pack(pady=10)
 
-# 创建输出框
 output_box = tk.Text(root, wrap=tk.WORD, width=80, height=20)
 output_box.pack(pady=10)
+output_box.bind('<Control-Key>', edit_text)
 
-# 启动Tkinter事件循环
 root.mainloop()
